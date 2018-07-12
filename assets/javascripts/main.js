4568fcf6b1e8e46f208dd37f9b2c58dd7f16ee29
zuonx <hjsfzzz@gmail.com>
2018-07-12 10:48:27 +0800

    🚀 Increase loading speed

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,36 +1,25 @@
-// Menu
-
-$().ready(function($) {
-  $(".btn_menu").on('click', function(e) {
-    $(".menu").toggleClass('menu_open');
-    e.preventDefault();
-    $(this).toggleClass('btn_menu--toggled');
-  });
-  $('.toggle_all').on('click', function(e) {
-    e.preventDefault();
-    $('.btn_menu').toggleClass('btn_menu--toggled');
-  });
-});
-
-// Fullpage
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-    anchors: ['Main Page', 'About', 'Case_1', 'Case_2', 'Case_3', 'Blog', 'Story', 'Contact'],
-    menu: '#menu',
-    scrollingSpeed: 600,
-    navigation: true,
-    navigationPosition: 'right',
-  });
-});
-
-// Back Top
-$(document).ready(function() {
-  $('#goToTop').click(function() {
-    $('html,body').animate({
-      scrollTop: 0
-    }, 'slow');
-  });
-});
+// // Menu
+//
+// $().ready(function($) {
+//   $(".btn_menu").on('click', function(e) {
+//     $(".menu").toggleClass('menu_open');
+//     e.preventDefault();
+//     $(this).toggleClass('btn_menu--toggled');
+//   });
+//   $('.toggle_all').on('click', function(e) {
+//     e.preventDefault();
+//     $('.btn_menu').toggleClass('btn_menu--toggled');
+//   });
+// });
+//
+// // Back Top
+// $(document).ready(function() {
+//   $('#goToTop').click(function() {
+//     $('html,body').animate({
+//       scrollTop: 0
+//     }, 'slow');
+//   });
+// });
 
 // China Time
 $.each($('.js--time'), function() {
@@ -41,16 +30,14 @@ $.each($('.js--time'), function() {
     }, 1000);
 });
 
-// About Swich
-$().ready(function($) {
-  $(".about_swich_en").on('click', function() {
-    $(".about_cn").removeClass('active');
-    $(".about_en").addClass('active');
-  });
-  $('.about_swich_cn').on('click', function() {
-    $(".about_en").removeClass('active');
-    $(".about_cn").addClass('active');
-  });
-});
-
-mediumZoom(document.images)
+// // About Swich
+// $().ready(function($) {
+//   $(".about_swich_en").on('click', function() {
+//     $(".about_cn").removeClass('active');
+//     $(".about_en").addClass('active');
+//   });
+//   $('.about_swich_cn').on('click', function() {
+//     $(".about_en").removeClass('active');
+//     $(".about_cn").addClass('active');
+//   });
+// });
