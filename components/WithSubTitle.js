d1f085801085606ae160f92161f5ef12a4d61510
左子祯 Zizhen <hjsfzzz@gmail.com>
2021-11-18 13:35:36 +0800

    Update WithSubTitle.js

--- components/WithSubTitle.js
+++ components/WithSubTitle.js
@@ -2,7 +2,7 @@ export default function IconTitle({ subtitle, title, icon }) {
   return (
     <div className="mt-12">
       <div className="-mb-10 text-xs text-gray-500 dark:text-gray-400 flex items-center space-x-1">
-        <i class={icon}></i>
+        <i className={icon}></i>
         <span>{subtitle}</span>
       </div>
       <h2 className="">{title}</h2>
