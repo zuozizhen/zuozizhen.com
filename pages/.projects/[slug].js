4709ed0dfcb1adc575315b9c500358f2c122a717
左子祯 <hjsfzzz@gmail.com>
2022-01-31 13:05:34 +0800

    fix

--- pages/.projects/[slug].js
+++ pages/.projects/[slug].js
@@ -1,42 +1,42 @@
-import { MDXRemote } from 'next-mdx-remote';
+// import { MDXRemote } from 'next-mdx-remote';
 
-import { getFiles, getFileBySlug } from '@/lib/mdx';
+// import { getFiles, getFileBySlug } from '@/lib/mdx';
 
-import BlogLayout from '@/layouts/blog';
-import MDXComponents from '@/components/MDXComponents';
+// import BlogLayout from '@/layouts/blog';
+// import MDXComponents from '@/components/MDXComponents';
 
-export default function Projects({ mdxSource, frontMatter }) {
-  return (
-    <BlogLayout frontMatter={frontMatter}>
-      <MDXRemote
-        {...mdxSource}
-        components={{
-          ...MDXComponents
-          // StaticTweet
-        }}
-      />
-    </BlogLayout>
-  );
-}
+// export default function Projects({ mdxSource, frontMatter }) {
+//   return (
+//     <BlogLayout frontMatter={frontMatter}>
+//       <MDXRemote
+//         {...mdxSource}
+//         components={{
+//           ...MDXComponents
+//           // StaticTweet
+//         }}
+//       />
+//     </BlogLayout>
+//   );
+// }
 
 
-export async function getStaticPaths() {
-  const projects = await getFiles('projects');
+// export async function getStaticPaths() {
+//   const projects = await getFiles('projects');
 
-  return {
-    paths: projects.map((s) => ({
-      params: {
-        slug: s.replace(/\.mdx/, '')
-      }
-    })),
-    fallback: false
-  };
-}
+//   return {
+//     paths: projects.map((s) => ({
+//       params: {
+//         slug: s.replace(/\.mdx/, '')
+//       }
+//     })),
+//     fallback: false
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  const projects = await getFileBySlug('projects', params.slug);
-  // const tweets = await getTweets(post.tweetIDs);
+// export async function getStaticProps({ params }) {
+//   const projects = await getFileBySlug('projects', params.slug);
+//   // const tweets = await getTweets(post.tweetIDs);
 
-  // return { props: { ...post, tweets } };
-  return { props: { ...projects } };
-}
+//   // return { props: { ...post, tweets } };
+//   return { props: { ...projects } };
+// }
