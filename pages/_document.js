f42976d056b8de6c32226ffb8a5e7ecbda79288b
左子祯 <hjsfzzz@gmail.com>
2022-04-26 22:23:10 +0800

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,12 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            rel="preload"
             href="/fonts/inter-var-latin.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
           />
           <link
-            rel="preload"
             href="/fonts/JetBrainsMono-Medium.woff2"
             as="font"
             type="font/woff2"
