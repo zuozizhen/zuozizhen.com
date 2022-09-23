0516ef554067013d85323f25c8c26dde664689c7
zuozizhen <hjsfzzz@gmail.com>
2022-09-23 23:27:45 +0800

    no message

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,22 +1,24 @@
 import Link from 'next/link';
 import useSWR from 'swr';
+import fetcher from '@/lib/fetcher';
 
 import ArrowMotion from '@/components/ArrowMotion';
+import slugify from 'slugify';
+import PageViews from '@/components/PageViews';
 
 const BlogPost = ({ title, summary, slug }) => {
   // const { data } = useSWR(`/api/views/${slug}`, fetcher);
-  // const views = data?.total;
 
   return (
-    <Link href={`/blog/${slug}`}>
+    <Link href={`/blog/${slug}`} passHref>
       <a className="w-full">
         <div className="mb-8 w-full">
           <div className="flex relative arrow-motion items-center mb-1 hover:text-gray-600 dark:hover:text-gray-500 transition">
-            <h4 className="font-bold">{title}</h4>
-            <ArrowMotion />
-            {/* <p className="text-gray-500 text-left md:text-right w-32 mb-4 md:mb-0">
-              {`${views ? new Number(views).toLocaleString() : '–––'} views`}
-            </p> */}
+            <div className="flex items-center">
+              <h4 className="font-bold">{title}</h4>
+              <ArrowMotion />
+            </div>
+            <PageViews slug={slug} />
           </div>
           <p className="prose text-gray-600 dark:text-gray-500">{summary}</p>
         </div>
