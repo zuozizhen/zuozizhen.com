a4371101941ab3aeb4647fb88d1d7d9d40b4d196
左子祯 Zizhen <hjsfzzz@gmail.com>
2024-03-28 19:36:26 +0800

    fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,21 +2,17 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
+export async function GET({ site }) {
   const posts = await getCollection('blog');
   return rss({
     title: '左子祯',
     description: '左子祯的个人网站',
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#contextsite
-    site: context.site,
+    site: site.origin, // 使用 site.origin 来确保我们获得完整的网站 URL
     items: posts.map((post) => ({
-      // Assumes all RSS feed item properties are in post frontmatter
-      ...post.data,
-      // Generate a `url` from each post `slug`
-      // This assumes all blog posts are rendered as `/blog/[slug]` routes
-      // https://docs.astro.build/en/guides/content-collections/#generating-pages-from-content-collections
-      link: `/blog/${post.slug}/`,
+      title: post.data.title,
+      description: post.data.snippet,
+      pubDate: post.data.publishDate,
+      link: `${site.origin}/blog/${post.slug}/`, // 使用完整的 URL 作为 link
     })),
   });
 }
